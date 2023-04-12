80970344433d6dee24343c4825472f8833c3051d
Alonso Solis Gerloni <alonsosolisgerloni@gmail.com>
2023-04-12 23:30:18 +0300

    cookie tracker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    CLARITY_ID: process.env.CLARITY_ID,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,60 @@ import {
   Contact,
   Footer,
 } from "@/components";
+import Head from "next/head";
+import CookieConsent from "react-cookie-consent";
+import { useEffect, useState } from "react";
+
+const CLARITY_ID = process.env.CLARITY_ID;
 
 export default function Home() {
+  const [cookieConsent, setCookieConsent] = useState(false);
+
+  function getCookie(name) {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith(name + "=")) {
+        return cookie.substring(name.length + 1);
+      }
+    }
+    return null;
+  }
+
+  useEffect(() => {
+    const consent = getCookie("alonsosgconsent");
+    setCookieConsent(consent === "true");
+  }, []);
+
   return (
     <div>
+      <Head>
+        {cookieConsent === true && (
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window,document,"clarity","script", "${CLARITY_ID}");
+          `,
+            }}
+          />
+        )}
+        <title>Alonso Solis Gerloni</title>
+      </Head>
+      <CookieConsent
+        location="bottom"
+        buttonText="Accept"
+        cookieName="alonsosgconsent"
+        expires={365}
+        onAccept={() => setCookieConsent(true)}
+      >
+        This website uses cookies to enhance the user experience. By using this
+        website, you consent to the use of cookies.
+      </CookieConsent>
       <Navbar />
       <Introduction />
       <Skillset />
